11937c18df1435e0fdf6cb5670d1b7889bcf2e17
Gal <gallib2@gmail.com>
2019-07-20 12:14:16 +0300

    coins move towards the score

--- Assets/Endless2DTerrain/Demos/Scripts/CoinPickup.cs
+++ Assets/Endless2DTerrain/Demos/Scripts/CoinPickup.cs
@@ -4,14 +4,50 @@ using Endless2DTerrain;
 
 public class CoinPickup : MonoBehaviour {
 
+    public Transform target;
+    public int speed = 5;
+    bool startMove = false;
+
+    private void Update()
+    {
+        if(startMove)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+            Material material = GetComponent<MeshRenderer>().material;
+            Color color = material.color;
+            color.a -= 0.15f; 
+            material.color = color;
+        }
+
+        Debug.Log("transform.position" + transform.position);
+        //Debug.Log("target.position" + target.position);
+
+        if(Mathf.Approximately(transform.position.y, target.position.y))
+        {
+            Debug.Log("in the if: ");
+            var terrainDisplayer = GameObject.FindObjectOfType(typeof(TerrainDisplayer)) as TerrainDisplayer;
+            startMove = false;
+            terrainDisplayer.PrefabManager.Pool.Remove(this.gameObject);
+        }
+
+        Debug.Log("startMove: " + startMove);
+    }
+
     void OnTriggerEnter(Collider item)
     {
+        var terrainDisplayer = GameObject.FindObjectOfType(typeof(TerrainDisplayer)) as TerrainDisplayer;
         //Assume only one terrain displayer at a time
-        var terrainDisplayer = GameObject.FindObjectOfType(typeof(TerrainDisplayer)) as TerrainDisplayer;        
-        if (terrainDisplayer != null && terrainDisplayer.PrefabManager != null && terrainDisplayer.PrefabManager.Pool != null)
+        if(item.name == "surfer_Sphere")
         {
-            Scoring.Instance.ScorePickup();
-            terrainDisplayer.PrefabManager.Pool.Remove(this.gameObject);
+            if (terrainDisplayer != null && terrainDisplayer.PrefabManager != null && terrainDisplayer.PrefabManager.Pool != null)
+            {
+                Scoring.Instance.ScorePickup();
+                startMove = true;
+
+
+                //terrainDisplayer.PrefabManager.Pool.Remove(this.gameObject);
+            }
         }
     }
 
